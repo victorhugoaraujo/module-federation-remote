243a82d500b780285863401cbf3ae0d23e243c41
Victor  Araújo <victor_araujo@gap.com>
2024-01-31 14:28:37 -0300

    change header

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    {/* <Nav>This is the nav in app SelfDesign</Nav> */}
+    <Nav>brand /season / year</Nav>
       <main className={styles.main}>
-        <h1>Self Desin Page</h1>
+        <h1>Self Desing Page</h1>
       </main>
-    {/* <Footer></Footer> */}
+    <Footer></Footer>
 
     </div>
   )
